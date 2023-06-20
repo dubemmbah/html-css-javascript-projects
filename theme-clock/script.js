59bfe73cd0b975c6a0cc9f552992673e7caefddb
dubemmbah <dubemmbah@gmail.com>
2023-06-20 09:08:40 +0100

    initial

---
+++ theme-clock/script.js
@@ -0,0 +1,93 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+const footer = document.querySelector(".footer");
+
+footer.innerHTML = `<footer class="footer">
+Developed with &hearts; by&nbsp<a href='https://linkedin.com/in/dubem-mbah/'> Chukwudubem Mbah</a> 
+`;
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  const clock = document.querySelector(".clock");
+
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    clock.style.backgroundColor = "#7777776c";
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    clock.style.backgroundColor = "black";
+    e.target.innerHTML = "Light mode";
+  }
+});
+
+const setTime = function () {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class='circle'>${date}</span>`;
+};
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setTime();
+setInterval(setTime, 1000);
